cf3ece544e9d56cbcd82e250cafe866e79b901d6
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2025-03-14 17:19:38 +0900

    update

--- python/250314/main.py
+++ python/250314/main.py
@@ -1,5 +1,6 @@
 # flask 프레임워크 로드 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
 
 # class 생성
 # Flask class는 파일의 이름을 인자값으로 대입
@@ -35,9 +36,65 @@ def second():
     # return "<i>second Page</i>"
     return render_template('second.html')
 
+# index page에서 보낸 데이터를 받는 주소
+# get방식으로 데이터를 받는 주소
+@app.route('/login')
+def get_login():
+    # request 메시지 확인 -> flask에 request 로드 
+    # request 안에 key가 args 되어있는 value을 확인 
+    print(request.args)
+    # 로그인 성공하는 조건?
+    # 유저가 입력한 아이디의 값이 'test'
+    # 유저가 입력한 패스워드의 값이 'asdf1234'
+    # 모두 만족을 할 때 로그인이 성공
+    # 둘 중 하나라도 만족을 못하면 로그인이 실패
+    # request는 데이터의 타입이? dict -> {'input_id':'test', 'input_pass' :'asdf1234'}
+    if request.args['input_id'] == 'test' and\
+          request.args['input_pass'] == 'asdf1234':
+        return "로그인 성공"
+    else:
+        return "로그인 실패"
+    # return 'login test'
 
+# post 방식으로 데이터를 보내는 주소를 생성 
+# /login_post -> [post]
+@app.route('/login_post', methods=['post'])
+def login_post():
+    # post 방식으로 보낸 데이터는 request 안에 form에 존재
+    print(f" POST 방식의 데이터 : {request.form} ")
+    # 유저가 보낸 아이디와 패스워드를 user_info.csv 파일에서 
+    # 해당하는 데이터가 존재하는가?
+    # user_info.csv 파일을 로드
+    post_id = request.form['post_id']
+    post_pass = request.form['post_pass'] 
+    user = pd.read_csv('user_info.csv')
+    # loc를 이용하여 인덱스의 조건식
+    # 조건식 -> user['id'] == post_id
+    #           user['pass'] == post_pass
+    # 두 개의 조건식을 모두 만족하는 데이터프레임의 길이
+    flag = user['id'] == post_id
+    flag2 = user['pass'] == int(post_pass)
+    print(f" user 데이터프레임 결과 : {user.loc[flag & flag2]}")
+    if len(user.loc[flag & flag2]):
+        # user 데이터프레임에서 id, pass가 모두 일치하는 name 값을 변수에 저장
+        user_name = user.loc[flag & flag2, 'name'].iloc[0]
+        print(f" 유저의 이름은 {user_name}")
+        # return "로그인 성공"
+        return render_template('main.html', user = user_name)
+    else:
+        return "로그인 실패"
+    # login_check = False
+    # for i in range(len(user)):
+    #     # i는 0, 1, 2, 3, .... 위치 값
+    #     if list(user.iloc[i].values ) == [post_id, int(post_pass)]:
+    #         # 특정 변수의 데이터를 True
+    #         login_check = True
+    # if login_check:
 
-
+    #     return "로그인 성공"
+    # else:
+    #     return "로그인 실패"
+    # return "test"
 
 
 # 웹 서버 오픈 
